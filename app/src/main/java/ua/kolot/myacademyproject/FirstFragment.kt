7e24adcef04447e8c9402286863c79e745204505
Мицинская Елизавета Григорьевна <mitsinskayae@atbmarket.com>
2020-11-19 16:43:56 +0200

    design for movie details

--- app/src/main/java/ua/kolot/myacademyproject/FirstFragment.kt
+++ app/src/main/java/ua/kolot/myacademyproject/FirstFragment.kt
@@ -1,5 +1,6 @@
 package ua.kolot.myacademyproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -25,7 +26,9 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            val intent = Intent(context, MovieDetailsActivity::class.java)
+            context?.startActivity(intent)
         }
     }
 }
